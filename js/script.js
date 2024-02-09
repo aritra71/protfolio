caa1d56fafe7ed501b499d35306e117e5a03b277
Aritra Sarkar <aritra.aiub@gmail.com>
2024-02-10 00:44:32 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*typing animation*/
+var typed = new Typed(".typing",{
+    strings:["","student","part time teacher","part time job holder"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", ()=>
+{
+    asideSelectionTogglerBtn();
+})
+function asideSelectionTogglerBtn()
+{
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    for(let i=0; i<totalSection; i++)
+    {
+        allSection[i].classList.toggle("open");
+    }
+}
